b2ca33d1f395c59acdab3e8a67ba01c99b361c74
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-11-28 19:56:13 -0500

    Numero de Contagios en Colombia

--- covid.py
+++ covid.py
@@ -5,3 +5,11 @@ Created on Sun Nov 28 17:33:07 2021
 
 @author: ivandavid
 """
+import pandas as pd
+
+url = "covid_22_noviembre.csv"
+data = pd.read_csv(url)
+
+# 1. Numero de casos de contagiados en el pais
+n_contagios = data.shape[0]
+print(f'\nCasos de Covid-19 en Colombia: {n_contagios}')
